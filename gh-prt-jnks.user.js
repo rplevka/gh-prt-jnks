b9419eeca88552d9bc94061301db7d59e721e4d7
Roman Plevka <rplevka@redhat.com>
2024-02-28 00:40:23 +0100

    add some comments

--- gh-prt-jnks.user.js
+++ gh-prt-jnks.user.js
@@ -12,10 +12,13 @@
 
 (function() {
     'use strict';
+    // get list of comment elements
     var comments = document.getElementsByClassName("timeline-comment");
     for (let i = 0; i < comments.length; i++) {
+        // locate the author element and filter those from our bot
         var el_author = comments[i].getElementsByClassName("author");
         if (el_author.length && el_author[0].innerHTML == botUsername) {
+            // we assume PRT bot comment contains `code`
             var els_code = document.evaluate(
                 ".//code",
                 comments[i],
@@ -25,6 +28,9 @@
             );
             var el_code = els_code.iterateNext();
             if(el_code){
+                /*  match code text against our pattern and optionally replace
+                    it with the <a> node
+                */
                 var replacement = el_code.innerHTML.replace(
                     comment_pattern,
                     `$1<a href='${jnks_job_url}/$2' target='_blank'>$2</a>`
